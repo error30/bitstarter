21bebfaf1943490e9f087ecaafc316cb9bd0ca86
error30 <mike.notification@gmail.com>
2013-07-13 18:06:07 +0000

    omg

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   var out = buffer;
-  buffer.write(fs.readFileSync('index.html', 'utf-8'));
+  fs.readFileSync('index.html', 'utf-8');
 
 
 
