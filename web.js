6bc7e7de0e6189cd70fc2408afa2c0d9f6b48aca
error30 <mike.notification@gmail.com>
2013-07-13 05:00:16 +0000

    Modified web.js

--- web.js
+++ web.js
@@ -1,6 +1,6 @@
 var express = require('express');
 
-var buffer = fs.readFileSync(index.html);
+var buffer = new Buffer(fs.readFileSync(index.html));
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
