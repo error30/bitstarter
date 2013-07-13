2c55684d1810c2c39f266db06d5c71165c2f2606
error30 <mike.notification@gmail.com>
2013-07-13 07:50:13 +0000

    modified web.js

--- web.js
+++ web.js
@@ -1,12 +1,14 @@
 var fs = require('fs');
 var express = require('express');
-var incoming = index.html;
-var buffer = new Buffer(fs.readFileSync(incoming));
+var buffer = new Buffer(str);
 var app = express.createServer(express.logger());
 
+fs.readFile('index.html', function (err, data) {
+if (err) throw err;
 
 app.get('/', function(request, response) {
   response.send(buffer);
+});
 });
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
